c5269995592899d9cfefed72d4bcb1a1db3f5225
Li Arolf Rey <liarolfrrey@gmail.com>
2017-09-15 17:00:08 +0800

    Add login component ui

--- src/frontend/components/Dashboard/index.jsx
+++ src/frontend/components/Dashboard/index.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
-import Header from './Header';
 import styled from 'styled-components';
+import Header from './Header';
 
 const StyledDiv = styled.div`
   background: #FAFAFA;
--- src/frontend/stores/TaskStore.js
+++ src/frontend/stores/TaskStore.js
@@ -12,55 +12,55 @@ class TaskStore {
     bidService;
 
     constructor() {
-        this.initialize();
-        this.fetchUsers();
-        this.fetchTasks();
-        this.fetchBids();
+      this.initialize();
+      this.fetchUsers();
+      this.fetchTasks();
+      this.fetchBids();
     }
 
     initialize() {
-        this.userService = app.service('/users');
-        this.taskService = app.service('/api/tasks');
-        this.bidService = app.service('/api/bids');
+      this.userService = app.service('/users');
+      this.taskService = app.service('/api/tasks');
+      this.bidService = app.service('/api/bids');
 
-        this.taskService.on('created', newTask => {
-            this.tasks.push(newTask);
-        });
-        this.bidService.on('created', newBid => {
-            this.bids.push(newBid);
-        })
+      this.taskService.on('created', newTask => {
+        this.tasks.push(newTask);
+      });
+      this.bidService.on('created', newBid => {
+        this.bids.push(newBid);
+      });
     }
 
     async fetchTasks() {
-        this.tasks = await this.taskService.find();
+      this.tasks = await this.taskService.find();
     }
 
     async fetchBids() {
-        this.bids = await this.bidService.find();
+      this.bids = await this.bidService.find();
     }
 
     async fetchUsers() {
-        this.users = await this.userService.find();
+      this.users = await this.userService.find();
     }
 
     async bid(userId, task, price) {
-        const data = {
-            taskId : task._id,
-            user : userId,
-            price,
-            date : new Date(Date.now())
-        };
-        await this.bidService.create(data);
+      const data = {
+        taskId: task._id,
+        user: userId,
+        price,
+        date: new Date(Date.now())
+      };
+      await this.bidService.create(data);
     }
 
     async postTask(description, minPrice, maxPrice) {
-        const data = {
-            description,
-            employer : ObjectId('5995c5c438070f179c609413'),
-            maxPrice,
-            minPrice,
-            dateIssued : new Date(Date.now())
-        };
-        await this.taskService.create(data);
+      const data = {
+        description,
+        employer: ObjectId('5995c5c438070f179c609413'),
+        maxPrice,
+        minPrice,
+        dateIssued: new Date(Date.now())
+      };
+      await this.taskService.create(data);
     }
 }
