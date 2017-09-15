9bbbd8de3fe519e7cdc0db00c5d4718ebde588f6
Brent Anthony R. Tudas <brent.tudas@gmail.com>
2017-09-15 21:37:20 +0800

    feat(Map With Many Users):

--- src/frontend/components/Map/Map.jsx
+++ src/frontend/components/Map/Map.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { compose, withProps, withState, withHandlers } from "recompose";
+// import {observer, inject} from 'mobx-react';
 // import FaAnchor from "react-icons/lib/fa/ancho";
 import {
   withScriptjs,
@@ -9,6 +10,29 @@ import {
   InfoWindow,
 } from "react-google-maps";
 
+const UserMarker = ({lat, lng, name, props}) => (
+    <Marker
+      position={{ lat: lat, lng: lng }}
+      onClick={props.onToggleOpen}
+    >
+      <InfoWindow onCloseClick={props.onToggleOpen}>
+        <div onClick={() => console.log('modal should open')}>
+          {/* <FaAnchor /> */}
+          { name }
+          {/* Controlled zoom: {props.zoom} */}
+        </div>
+      </InfoWindow>
+    </Marker>
+);
+
+const markers = [
+    { lat: 10.7139343, lng: 122.5516674, name : 'Brent Anthony Tudas'},
+    { lat: 10.7139453, lng: 122.5516734, name : 'Glyda Mae Torres'},
+    { lat: 10.7139593, lng: 122.5516834, name : 'Li Arolf Rey'},
+];
+
+
+
 const MapWithControlledZoom = compose(
   withProps({
     googleMapURL: "https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places",
@@ -16,7 +40,7 @@ const MapWithControlledZoom = compose(
     containerElement: <div style={{ height: `800px` }} />,
     mapElement: <div style={{ height: `100%` }} />,
   }),
-  withState('zoom', 'onZoomChange', 8),
+  withState('zoom', 'onZoomChange', 20),
   withHandlers(() => {
     const refs = {
       map: undefined,
@@ -35,23 +59,14 @@ const MapWithControlledZoom = compose(
   withGoogleMap
 )(props =>
   <GoogleMap
-    defaultCenter={{ lat: -34.397, lng: 150.644 }}
+    defaultCenter={{ lat: 10.7139343, lng: 122.5516674 }}
     zoom={props.zoom}
     ref={props.onMapMounted}
     onZoomChanged={props.onZoomChanged}
   >
-    <Marker
-      position={{ lat: -34.397, lng: 150.644 }}
-      onClick={props.onToggleOpen}
-    >
-      <InfoWindow onCloseClick={props.onToggleOpen}>
-        <div>
-          {/* <FaAnchor /> */}
-          {" "}
-          Controlled zoom: {props.zoom}
-        </div>
-      </InfoWindow>
-    </Marker>
+  
+  
+    {markers.map(marker => <UserMarker props={props} lat={marker.lat} lng={marker.lng} name={marker.name} />)}
   </GoogleMap>
 );
 
