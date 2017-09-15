b30112d174b2dec23dc1448020efa54cefa4abc0
Li Arolf Rey <liarolfrrey@gmail.com>
2017-09-16 00:45:22 +0800

    Fix PaymentGateway

--- src/frontend/components/PaymentGateway/style.js
+++ src/frontend/components/PaymentGateway/style.js
@@ -1,67 +1,77 @@
 import styled from 'styled-components';
 
+const StyledDiv = styled.div`
+  display: flex;
+  justify-content: center;
+  width: 100%;
+  text-align: center;
+`
+
 const StyledComponent = styled.div`
-margin: 0;
-font-family: 'Varela Round', sans-serif;
-text-align: center;
+  margin: 0;
+  font-family: 'Varela Round', sans-serif;
+  text-align: center;
 `
 
 const StyledHeader = styled.div`
-background: #5D85A8;
-height: 60px;
-text-align: center;
+  background: #5D85A8;
+  height: 60px;
+  text-align: center;
 `
 
-const StyledDiv = styled.div`
-a {
-  line-height: 60px;
-  font-size: 2em;
-  text-decoration: none;
-  color: white;
-  margin-left: -37px;
-}
-h1 {
-  font-size: 2em;
-}
-input:nth-child(even) {
-  margin-bottom: 40px;
-}
+const PaymentContainer = styled.div`
+  width: 50%;
 
-h5 {
-  margin-top: 0;
-}
+
+  a {
+    line-height: 60px;
+    font-size: 2em;
+    text-decoration: none;
+    color: white;
+    margin-left: -37px;
+  }
+  h1 {
+    font-size: 2em;
+  }
+  input:nth-child(even) {
+    margin-bottom: 40px;
+  }
+
+  h5 {
+    margin-top: 0;
+  }
 `
 
 const StyledField = styled.div`
-background: lightgray;
-color: black;
-border: none;
-text-align: center;
-font-size: 1.3em;
-padding: 20px;
-border-radius: 5px;
+  background: lightgray;
+  color: black;
+  border: none;
+  text-align: center;
+  font-size: 1.3em;
+  padding: 20px;
+  border-radius: 5px;
 `
 
 const StyledMenu = styled.div`
-float: left;
-margin-left: 5px;
-padding: 5px;
-margin-top: 8px;
-display: inline-flex;
-border-radius: 5px;
-cursor: pointer;
+  float: left;
+  margin-left: 5px;
+  padding: 5px;
+  margin-top: 8px;
+  display: inline-flex;
+  border-radius: 5px;
+  cursor: pointer;
 `
 
 const StyledSubmit = styled.div`
-background: #5F94C6;
-color: white;
-padding: 10px;
-font-size: 1.2em;
-width: 250px;
-padding: 12px 15px;
-border-radius: 3px;
-border: none;
-font-weight: bold;
+  background: #5F94C6;
+  color: white;
+  padding: 10px;
+  font-size: 1.2em;
+  width: 90%;
+  margin: auto;
+  border-radius: 3px;
+  border: none;
+  font-weight: bold;
 `
 
 const StyledContainer = styled.div`
@@ -71,4 +81,4 @@ const StyledContainer = styled.div`
 
 `
 
-export { StyledComponent, StyledContainer, StyledHeader, StyledDiv, StyledField, StyledMenu, StyledSubmit };
+export { StyledComponent, StyledContainer, StyledHeader, PaymentContainer, StyledField, StyledMenu, StyledSubmit, StyledDiv };
